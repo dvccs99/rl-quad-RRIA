1e6421d6dae68ff9ad400707ec18c19e5fdb8375
Daniel-Salvador <dvccs@softex.cin.ufpe.br>
2024-11-09 19:59:52 -0300

    REFACTOR: custom env

--- custom_env.py
+++ custom_env.py
@@ -1,112 +1,60 @@
-from typing import Dict, Tuple, Union
+from typing import Union
 import numpy as np
-from gymnasium import utils
 from gymnasium.envs.mujoco import MujocoEnv
 from gymnasium.spaces import Box
 
 
-DEFAULT_CAMERA_CONFIG = {
-    "distance": 4.0,
-}
+DEFAULT_CAMERA_CONFIG = {"distance": 4.0}
 
 
-class QuadEnv(MujocoEnv, utils.EzPickle):
+class QuadEnv(MujocoEnv):
     """
     """
 
-    metadata = {
-        "render_modes": [
-            "human",
-            "rgb_array",
-            "depth_array"
-        ],
-    }
-
-    def __init__(
-        self,
-        xml_file: str = "ant.xml",
-        frame_skip: int = 5,
-        default_camera_config: Dict[str, Union[float, int]] = DEFAULT_CAMERA_CONFIG,
-        forward_reward_weight: float = 1,
-        ctrl_cost_weight: float = 0.5,
-        contact_cost_weight: float = 5e-4,
-        healthy_reward: float = 1.0,
-        main_body: Union[int, str] = 1,
-        terminate_when_unhealthy: bool = True,
-        healthy_z_range: Tuple[float, float] = (0.2, 1.0),
-        contact_force_range: Tuple[float, float] = (-1.0, 1.0),
-        reset_noise_scale: float = 0.1,
-        exclude_current_positions_from_observation: bool = True,
-        include_cfrc_ext_in_observation: bool = True,
-    ):
-        utils.EzPickle.__init__(
-            self,
-            xml_file,
-            frame_skip,
-            default_camera_config,
-            forward_reward_weight,
-            ctrl_cost_weight,
-            contact_cost_weight,
-            healthy_reward,
-            main_body,
-            terminate_when_unhealthy,
-            healthy_z_range,
-            contact_force_range,
-            reset_noise_scale,
-            exclude_current_positions_from_observation,
-            include_cfrc_ext_in_observation,
-        )
-
-        self._forward_reward_weight = forward_reward_weight
-        self._ctrl_cost_weight = ctrl_cost_weight
-        self._contact_cost_weight = contact_cost_weight
-        self._healthy_reward = healthy_reward
-        self._terminate_when_unhealthy = terminate_when_unhealthy
-        self._healthy_z_range = healthy_z_range
-        self._contact_force_range = contact_force_range
-        self._main_body = main_body
-        self._reset_noise_scale = reset_noise_scale
-
-        self._exclude_current_positions_from_observation = (
-            exclude_current_positions_from_observation
-        )
-      
-        self._include_cfrc_ext_in_observation = include_cfrc_ext_in_observation
-
-        MujocoEnv.__init__(
-            self,
-            xml_file,
-            frame_skip,
-            observation_space=None,  # needs to be defined after
-            default_camera_config=default_camera_config,
-            render_mode='human',
-        )
-
-        self.metadata = {
-            "render_modes": [
+    metadata = {"render_modes": [
                 "human",
                 "rgb_array",
-                "depth_array",
-                "rgbd_tuple",
-            ],
-            "render_fps": int(np.round(1.0 / self.dt)),
-        }
+                "depth_array"]}
+
+    def __init__(self, env_parameters, mujoco_parameters):
+        self._forward_reward_weight = env_parameters["forward_reward_weight"]
+        self._ctrl_cost_weight = env_parameters["ctrl_cost_weight"]
+        self._contact_cost_weight = env_parameters["contact_cost_weight"]
+        self._healthy_reward = env_parameters["healthy_reward"]
+        self._terminate_when_unhealthy = env_parameters["terminate_when_unhealthy"]
+        self._healthy_z_range = env_parameters["healthy_z_range"]
+        self._contact_force_range = env_parameters["contact_force_range"]
+        self._reset_noise_scale = env_parameters["reset_noise_scale"]
+        self._exclude_current_positions_from_observation = env_parameters["exclude_current_positions_from_observation"]
+        self._include_cfrc_ext_in_observation = env_parameters["include_cfrc_ext_in_observation"]
+        self._main_body: Union[int, str] = 1
+
+        # default_camera_config: Dict[str, Union[float, int]] = DEFAULT_CAMERA_CONFIG,
+
+        MujocoEnv.__init__(self,
+                           model_path=mujoco_parameters['xml_file'],
+                           frame_skip=mujoco_parameters['frame_skip'],
+                           observation_space=mujoco_parameters['observation_space'],  # needs to be defined after
+                           default_camera_config=DEFAULT_CAMERA_CONFIG,
+                           render_mode=mujoco_parameters['render_mode'])
+
+        self.metadata = {"render_modes": [
+                         "human",
+                         "rgb_array",
+                         "depth_array",
+                         "rgbd_tuple"],
+                         "render_fps": int(np.round(1.0 / self.dt))}
 
         obs_size = self.data.qpos.size + self.data.qvel.size
-        obs_size -= 2 * exclude_current_positions_from_observation
-        obs_size += self.data.cfrc_ext[1:].size * include_cfrc_ext_in_observation
+        obs_size -= 2 * self._exclude_current_positions_from_observation
+        obs_size += self.data.cfrc_ext[1:].size * self._include_cfrc_ext_in_observation
 
-        self.observation_space = Box(
-            low=-np.inf, high=np.inf, shape=(obs_size,), dtype=np.float64
-        )
+        self.observation_space = Box(low=-np.inf, high=np.inf, shape=(obs_size, ), dtype=np.float64)
 
-        self.observation_structure = {
-            "skipped_qpos": 2 * exclude_current_positions_from_observation,
-            "qpos": self.data.qpos.size
-            - 2 * exclude_current_positions_from_observation,
-            "qvel": self.data.qvel.size,
-            "cfrc_ext": self.data.cfrc_ext[1:].size * include_cfrc_ext_in_observation,
-        }
+        self.observation_structure = {"skipped_qpos": 2 * self._exclude_current_positions_from_observation,
+                                      "qpos": self.data.qpos.size - 2 * self._exclude_current_positions_from_observation,
+                                      "qvel": self.data.qvel.size,
+                                      "cfrc_ext": self.data.cfrc_ext[1:].size * self._include_cfrc_ext_in_observation}
 
     @property
     def healthy_reward(self):
@@ -125,9 +73,7 @@ class QuadEnv(MujocoEnv, utils.EzPickle):
 
     @property
     def contact_cost(self):
-        contact_cost = self._contact_cost_weight * np.sum(
-            np.square(self.contact_forces)
-        )
+        contact_cost = self._contact_cost_weight * np.sum(np.square(self.contact_forces))
         return contact_cost
 
     @property
@@ -173,12 +119,10 @@ class QuadEnv(MujocoEnv, utils.EzPickle):
 
         reward = rewards - costs
 
-        reward_info = {
-            "reward_forward": forward_reward,
-            "reward_ctrl": -ctrl_cost,
-            "reward_contact": -contact_cost,
-            "reward_survive": healthy_reward,
-        }
+        reward_info = {"reward_forward": forward_reward,
+                       "reward_ctrl": -ctrl_cost,
+                       "reward_contact": -contact_cost,
+                       "reward_survive": healthy_reward}
 
         return reward, reward_info
 
@@ -200,12 +144,8 @@ class QuadEnv(MujocoEnv, utils.EzPickle):
         noise_high = self._reset_noise_scale
 
         qpos = self.init_qpos + self.np_random.uniform(
-            low=noise_low, high=noise_high, size=self.model.nq
-        )
-        qvel = (
-            self.init_qvel
-            + self._reset_noise_scale * self.np_random.standard_normal(self.model.nv)
-        )
+            low=noise_low, high=noise_high, size=self.model.nq)
+        qvel = (self.init_qvel + self._reset_noise_scale * self.np_random.standard_normal(self.model.nv))
         self.set_state(qpos, qvel)
 
         observation = self._get_obs()
@@ -213,8 +153,6 @@ class QuadEnv(MujocoEnv, utils.EzPickle):
         return observation
 
     def _get_reset_info(self):
-        return {
-            "x_position": self.data.qpos[0],
-            "y_position": self.data.qpos[1],
-            "distance_from_origin": np.linalg.norm(self.data.qpos[0:2], ord=2),
-        }
+        return {"x_position": self.data.qpos[0],
+                "y_position": self.data.qpos[1],
+                "distance_from_origin": np.linalg.norm(self.data.qpos[0:2], ord=2)}
--- first_run.py
+++ first_run.py
@@ -1,30 +1,37 @@
 from custom_env import QuadEnv
 from stable_baselines3 import SAC
 
+env_parameters = {
+    'forward_reward_weight': 1,
+    'ctrl_cost_weight': 0.05,
+    'contact_cost_weight': 5e-4,
+    'healthy_reward': 1,
+    'main_body': 1,
+    'healthy_z_range': (0.195, 0.75),
+    'include_cfrc_ext_in_observation': True,
+    'exclude_current_positions_from_observation': False,
+    'terminate_when_unhealthy': True,
+    'reset_noise_scale': 0.1,
+    'contact_force_range': (-1.0, 1.0)
+}
 
-env = QuadEnv(
-    xml_file='./mujoco_menagerie/boston_dynamics_spot/scene.xml',
-    forward_reward_weight=1,
-    ctrl_cost_weight=0.05,
-    contact_cost_weight=5e-4,
-    healthy_reward=1,
-    main_body=1,
-    healthy_z_range=(0.195, 0.75),
-    include_cfrc_ext_in_observation=True,
-    exclude_current_positions_from_observation=False,
-    reset_noise_scale=0.1,
-    # frame_skip=1
-)
+mujoco_parameters = {
+    'xml_file': './robots/boston_dynamics_spot/scene.xml',
+    'frame_skip': 50,
+    'observation_space': None,  # needs to be defined after
+    'default_camera_config': 'default_camera_config',
+    'render_mode': 'human'
+}
+
+env = QuadEnv(env_parameters, mujoco_parameters)
 
 model = SAC("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=2000)
 
 vec_env = model.get_env()
 obs = vec_env.reset()
-for i in range(1000):
+for i in range(10000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = vec_env.step(action)
     vec_env.render("human")
 env.close()
-
-print(model)
